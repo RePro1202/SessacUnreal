7c3ff8c188040749f48940072f3963745706ec14
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-08-24 18:06:49 +0900

    cpp / 디버깅 메크로 수정

--- FirstProject/Source/FirstProject/GameInfo.h
+++ FirstProject/Source/FirstProject/GameInfo.h
@@ -15,17 +15,15 @@
 // log 출력 관련 작업
 DECLARE_LOG_CATEGORY_EXTERN(FirstProject, Log, All);
 
-#define LOG_CALLINFO FString(__FUNCTION__) + TEXT("[") + FString::FromInt(__LINE__) + TEXT("]")
-
+#define   LOG_CALLINFO   (FString(__FUNCTION__) + TEXT("[") + FString::FromInt(__LINE__) + TEXT("]"))
 // %s : 문자열을 받아와서 그 문자렬로 채워준다.
 // %s는 FString을 바로 넣어줄 수 없다. 앞에 *을 붙여서 FString이 
 // 가지고있는 문자열의 주소를 가지고 오고 그 주소를 넣어주어야 한다.
 // 
 // %d : 정수를 받아와서 그 정수를 문자열로 만들어서 채워준다.
 // %f : 실수를 받아와서 그 실수를 문자열로 만들어서 채워준다.
-#define LOG(Format, ...) UE_LOG(FirstProject, Warning, TEXT("%s : %s"), *LOG_CALLINFO, *FString::Printf(Format, ##__VARGS__))
-
-#define LOGSTRING(str) UE_LOG(FirstProject, Warning, TEXT("%s : %s"), *LOG_CALLINFO, *str)
+#define   LOG(Format, ...)   UE_LOG(FirstProject, Warning, TEXT("%s : %s"), *LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+#define   LOGSTRING(str)      UE_LOG(FirstProject, Warning, TEXT("%S : %S"), *LOG_CALLINFO, *str)
 
 /**
  * 
