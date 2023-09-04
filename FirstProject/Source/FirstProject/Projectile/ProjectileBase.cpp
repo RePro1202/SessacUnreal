7f6631a7101cce42b33725ea9779f93f91cb9b81
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-09-04 12:05:01 +0900

    cpp / 투사체 액터 클래스 추가.
    1. ProjectileMovement 컴포넌트

---
+++ FirstProject/Source/FirstProject/Projectile/ProjectileBase.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectileBase.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mBody = CreateDefaultSubobject<UBoxComponent>(TEXT("Body"));
+	mMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	mMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Movement"));
+
+	SetRootComponent(mBody);
+
+	mMesh->SetupAttachment(mBody);
+	mMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	mBody->SetCollisionProfileName(TEXT("BlockAll"));
+
+	mMovement->SetUpdatedComponent(mBody);
+
+	mMovement->InitialSpeed = 1000.f;
+	mMovement->OnProjectileStop.AddDynamic(this, &AProjectileBase::ProjectileStop);
+}
+
+// Called when the game starts or when spawned
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AProjectileBase::ProjectileStop(const FHitResult& ImpactResult)
+{
+
+
+}
+
