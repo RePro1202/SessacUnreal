9799a4554ab39ccaaceb2c35714b2ca033e2757d
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-09-05 12:48:45 +0900

    고블린 캐릭터 에셋 추가.

---
+++ FirstProject/Source/FirstProject/AI/AIGoblinPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GameInfo.h"
+#include "AIPawn.h"
+#include "AIGoblinPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_API AAIGoblinPawn : public AAIPawn
+{
+	GENERATED_BODY()
+	
+};
