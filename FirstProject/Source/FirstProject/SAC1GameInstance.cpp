a2464c19e70c9dc3240e3cc2bae0cc4d52eef063
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-09-06 12:30:43 +0900

    cpp / 게임 인스턴스 생성 및 프로젝트 세팅 변경

---
+++ FirstProject/Source/FirstProject/SAC1GameInstance.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SAC1GameInstance.h"
+
+USAC1GameInstance::USAC1GameInstance()
+{
+
+}
+
+void USAC1GameInstance::Init()
+{
+	Super::Init();
+
+
+}
