b4092c21595770a935896ca94c41f4bd1013bd67
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-09-08 14:28:26 +0900

    cpp / 인공지능 거리체크 데코레이터 추가.
    1. 타겟(플레이어)와 거리체크 데코레이터 클래스 추가.
    2. 데이터 테이블 인식 거리 값 수정.
    3. 위 내용 비헤이비어 트리에 적용.

---
+++ FirstProject/Source/FirstProject/AI/AIModule/BTDecorator_CheckTargetDistance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../../GameInfo.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_CheckTargetDistance.generated.h"
+
+UENUM(BlueprintType)
+enum class ECheckDistanceType : uint8
+{
+	Attack,
+	Interaction
+};
+
+UCLASS()
+class FIRSTPROJECT_API UBTDecorator_CheckTargetDistance : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UBTDecorator_CheckTargetDistance();
+
+protected:
+	UPROPERTY(Category = Component, EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	ECheckDistanceType mCheckType;
+	
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const;
+	
+};
